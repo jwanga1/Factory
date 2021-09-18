f69b207673f8364abdc6ba1d0899ef08103e899e
jwanga1 <75241556+jwanga1@users.noreply.github.com>
2021-09-18 15:47:43 -0400

    Add files via upload

---
+++ (factory method)/Client.java
@@ -0,0 +1,30 @@
+public class Client {
+    private static Dialog dialog;
+
+    public static void main(String[] args) {
+        configure();
+        run();
+    }
+
+    /**
+     * The concrete factory is usually chosen depending on configuration or
+     * environment options.
+     */
+    static void configure() {
+        if (System.getProperty("os.name").equals("Windows 10")) {
+            dialog = new WindowsDialog();
+        } else {
+            dialog = new HtmlDialog();
+        }
+    }
+
+    /**
+     * All of the client code should work with factories and products through
+     * abstract interfaces. This way it does not care which factory it works
+     * with and what kind of product it returns.
+     */
+    static void run() {
+        dialog.renderWindow();
+    }
+
+}
